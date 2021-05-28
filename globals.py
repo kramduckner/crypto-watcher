d2b1cc2eade4099ef75ec2af1d4f6279d071debd
Mark Buckner <buckner.a.mark@gmail.com>
2021-05-28 08:27:55 -0600

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,4 +1,3 @@
 selected_symbol_index=0
-symbols=['ADA', 'BTC', 'DOGE', 'XCH', 'BAT'] #list
-symbols_to_ticker={'ADA': 'adausdt', 'BTC':'btcusdt', 'DOGE':'dogeusdt', 'XCH': 'xchusdt', 'BAT': 'batusdt'} #dictionary
-
+symbols=['ADA', 'BTC', 'DOGE', 'BAT', 'TEZOS', 'ALGO']
+symbols_to_ticker={'ADA': 'adausdt', 'BTC':'btcusdt', 'DOGE':'dogeusdt', 'BAT': 'batusdt', 'TEZOS':'xtzusdt', 'ALGO':'algobusd'} 
